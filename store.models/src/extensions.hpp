101a7cec3872b609cc530318eba88da04e393ad7
once-ler <htao@eztier.com>
2018-10-29 16:06:34 -0400

    Add trim function

--- store.models/src/extensions.hpp
+++ store.models/src/extensions.hpp
@@ -87,6 +87,20 @@ namespace store {
       return move(str);
     }
 
+    std::string& ltrim(std::string& str, const std::string& chars = "\t\n\v\f\r ") {
+      str.erase(0, str.find_first_not_of(chars));
+      return str;
+    }
+ 
+    std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ") {
+      str.erase(str.find_last_not_of(chars) + 1);
+      return str;
+    }
+ 
+    std::string& trim(std::string& str, const std::string& chars = "\t\n\v\f\r ") {
+      return ltrim(rtrim(str, chars), chars);
+    }
+
     inline string generate_uuid(int vers = UUID_MAKE_V4) {
       uuid id;
       id.make(vers);
