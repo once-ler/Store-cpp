9d9cc113246dbd32054e62794088b2a6fa95f09f
once-ler <htao@eztier.com>
2019-07-05 14:41:51 -0400

    Additional parsing for wrappers for libevent test

--- test/005-parse-form.cpp
+++ test/005-parse-form.cpp
@@ -1,11 +1,19 @@
 /*
 cp ../sample-post-data.txt bin
-g++ -std=c++14 -Wall -I ../../ -o bin/parse-form ../005-parse-form.cpp -levent -lpthread
+g++ -std=c++14 -Wall -I ../../ -I ../../../json/single_include/nlohmann -o bin/parse-form ../005-parse-form.cpp -levent -lpthread
 */
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <evhttp.h>
+#include <regex>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+// An implementation of regex replace in c is here:
+// https://gist.github.com/tanbro/4379692
 
 char* readFile(const char* fname) {
   FILE* fdata = fopen(fname, "rb");
@@ -33,11 +41,34 @@ auto main(int argc, char* argv[]) -> int {
   char* buf = readFile("sample-post-data.txt");
   struct evkeyvalq* headers = (struct evkeyvalq*) malloc(sizeof(struct evkeyvalq));
 
+  std::regex rgx("(%2F)");
+
   evhttp_parse_query_str(buf, headers);
 
   struct evkeyval* kv = headers->tqh_first;
   while (kv) {
-    printf("%s: %s\n", kv->key, kv->value);
+
+    std::string key(kv->key);
+    key = std::regex_replace(key, rgx, "/");
+
+    json val = kv->value[0] == '[' ? json::parse(kv->value) : kv->value;
+
+    // Wrappers parsing.
+    if (kv->value[0] == '[') {
+      
+      for (const auto o : val) {
+        if (o["code"].get<std::string>()[0] == '{') {
+          json code = json::parse(o.value("code", "{}"));
+          for (auto& el : code.items()) {
+            std::string v = el.value()["code"];
+            if (v.size() > 0)
+              std::cout << el.key() << " : " << el.value()["code"] << std::endl;
+          }
+        }
+      }
+    }
+
+    // std::cout << key << ": " << val.dump(2) << std::endl;
     kv = kv->next.tqe_next;
   }
   
