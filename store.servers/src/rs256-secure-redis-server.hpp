e2bfe6f20d6d5459497f6d0346aa647eb3bc0070
once-ler <htao@eztier.com>
2019-09-01 15:05:48 -0400

    Include RS256 http server with redis support

---
+++ store.servers/src/rs256-secure-redis-server.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cpp_redis/cpp_redis>
+#include "store.servers/src/rs256-secure-server.hpp"
+#include "store.models/src/ioc/service_provider.hpp"
+
+using namespace store::common;
+
+namespace ioc = store::ioc;
+
+namespace store::servers {
+
+  class RS256SecureRedisServer : public RS256SecureServer {
+    using RS256SecureServer::RS256SecureServer;
+    
+  public:
+    RS256SecureRedisServer() : RS256SecureServer() {
+      redis_client = ioc::ServiceProvider->GetInstance<cpp_redis::client>();
+    }
+
+  private:
+    shared_ptr<cpp_redis::client> redis_client;
+    
+    void publish(const string& session_id, const string& message) {
+      redis_client->publish(session_id, message);    
+      redis_client->sync_commit();
+    }
+  };
+
+}
