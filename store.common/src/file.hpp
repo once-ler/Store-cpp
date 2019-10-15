e45dfb69c544c810f6680486616227b7e60cf0c1
once-ler <htao@eztier.com>
2019-10-15 11:00:14 -0400

    Additional functions for file io

--- store.common/src/file.hpp
+++ store.common/src/file.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <stdio.h>  /* defines FILENAME_MAX */
+#include <unistd.h> // getcwd
 #include <fstream>
+#include <boost/filesystem.hpp>
 
 using namespace std;
 
@@ -20,4 +23,34 @@ namespace store::common {
 
     return move(str);
   }
+
+  void dumpFile(const string& outName, const string& content) {
+    ofstream of;
+    of.open(outName);
+    of << content;
+    of.close();
+  }
+
+  auto getAllFilesInDir = [](const string& dirName) -> vector<string> {
+    namespace fs = boost::filesystem;
+    vector<string> list;
+
+    fs::path p(dirName);
+    fs::recursive_directory_iterator end;
+
+    for (fs::recursive_directory_iterator i(p); i != end; ++i) {
+      const fs::path cp = (*i);
+      if (boost::filesystem::is_regular_file(cp))
+        list.push_back(cp.string());
+    }
+    return list;
+  };
+
+  auto getCurrentWorkingDir = []() -> string {
+    char buff[FILENAME_MAX];
+    getcwd( buff, FILENAME_MAX );
+    string current_working_dir(buff);
+    return current_working_dir;
+  };
+
 }
