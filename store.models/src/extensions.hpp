6bc8c9416724155cd6204c6fe48166c9829115c0
once-ler <htao@eztier.com>
2021-01-17 15:51:25 -0500

    Add string_replace function

--- store.models/src/extensions.hpp
+++ store.models/src/extensions.hpp
@@ -212,5 +212,17 @@ namespace store {
       return upper_case(str_to_hex(decoded));
     }
 
+    // Author: https://stackoverflow.com/users/309308/michael-mrozek
+    // https://stackoverflow.com/questions/3418231/replace-part-of-a-string-with-another-string
+    void string_replace(std::string& str, const std::string& from, const std::string& to) {
+      if(from.empty())
+        return;
+      size_t start_pos = 0;
+      while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+      }
+    }
+
   }
 }
